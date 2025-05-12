5ce98663fb6662aa8ec6bd4db633cc2b01befa80
harun708280 <harun708280@gmail.com>
2025-05-13 03:03:54 +0600

    update dashboard

--- src/app/dashboard/transactions/page.jsx
+++ src/app/dashboard/transactions/page.jsx
@@ -2,20 +2,30 @@
 import { Printer, MoreVertical, Edit, Delete, Share } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { useUser } from "@clerk/nextjs";
 import Loader from "@/components/global/Loader";
-import { useAllTransactionByEmailQuery, useDeleteTransactionMutation } from "@/redux/Api/transaction";
+import {
+  useAllTransactionByEmailQuery,
+  useDeleteTransactionMutation,
+} from "@/redux/Api/transaction";
 import { AnimatedModalDemo } from "./component/AnimatedModalDemo";
 import MyModal from "../component/Dialog";
-import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs";
 
 export default function TransactionsPage() {
   const { user, isSignedIn } = useUser();
@@ -27,15 +37,19 @@ export default function TransactionsPage() {
 
   const { data: transactions, isLoading } = useAllTransactionByEmailQuery({
     email,
-    type: selectedTab === "Income" ? "income" : selectedTab === "Expense" ? "expense" : "",
+    type:
+      selectedTab === "Income"
+        ? "income"
+        : selectedTab === "Expense"
+        ? "expense"
+        : "",
   });
+
   const [deleteTransaction] = useDeleteTransactionMutation();
 
   if (!isSignedIn || isLoading) {
     return <Loader />;
   }
-
-  
 
   const confirmDeleteTransaction = (id) => {
     toast("Are you sure you want to delete this?", {
@@ -60,41 +74,88 @@ export default function TransactionsPage() {
     }
   };
 
+  const handleExportPDF = async () => {
+    const input = document.getElementById("transactions-table");
+    if (!input) return;
+
+    const canvas = await html2canvas(input);
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("l", "pt", "a4");
+    const imgProps = pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("transactions.pdf");
+  };
+
   return (
     <Tabs defaultValue="all" onValueChange={setSelectedTab}>
-      <div className="p-6  min-h-screen text-black  w-full mx-auto rounded-2xl">
+      <div className="p-6 min-h-screen text-black w-full mx-auto rounded-2xl">
         <div className="p-4 shadow bg-white rounded-md">
           <h2 className="text-xl font-bold mb-4">Transactions</h2>
 
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 justify-between items-center mb-4">
             <TabsList className="grid w-1/3 md:grid-cols-3 bg-gradient-to-r from-blue-900 to-blue-500">
-              <TabsTrigger value="all"className={`${selectedTab==='all'?'text-black ':'text-white'}`}>
+              <TabsTrigger
+                value="all"
+                className={`${
+                  selectedTab === "all" ? "text-black" : "text-white"
+                }`}
+              >
                 All Transactions
               </TabsTrigger>
-              <TabsTrigger value="Income" className={`${selectedTab==='Income'?'text-black':'text-white'}`}>
+              <TabsTrigger
+                value="Income"
+                className={`${
+                  selectedTab === "Income" ? "text-black" : "text-white"
+                }`}
+              >
                 Income Only
               </TabsTrigger>
-              <TabsTrigger value="Expense" className={`${selectedTab==='Expense'?'text-black':'text-white'}`}>
+              <TabsTrigger
+                value="Expense"
+                className={`${
+                  selectedTab === "Expense" ? "text-black" : "text-white"
+                }`}
+              >
                 Expense Only
               </TabsTrigger>
             </TabsList>
             <div className="flex items-center gap-4">
               <AnimatedModalDemo />
-              <button className="bg-gradient-to-r from-blue-900 to-blue-500 text-white px-4 py-2 rounded-md flex items-center gap-3">
+              <button
+                onClick={handleExportPDF}
+                className="bg-gradient-to-r from-blue-900 to-blue-500 text-white px-4 py-2 rounded-md flex items-center gap-3"
+              >
                 <Printer /> Export
               </button>
             </div>
           </div>
 
-         
           <TabsContent value="all">
-            <TransactionTable transactions={transactions} confirmDeleteTransaction={confirmDeleteTransaction} setIsOpen={setIsOpen} setTnxId={setTnxId} />
+            <TransactionTable
+              transactions={transactions}
+              confirmDeleteTransaction={confirmDeleteTransaction}
+              setIsOpen={setIsOpen}
+              setTnxId={setTnxId}
+            />
           </TabsContent>
           <TabsContent value="Income">
-            <TransactionTable transactions={transactions} confirmDeleteTransaction={confirmDeleteTransaction} setIsOpen={setIsOpen} setTnxId={setTnxId} />
+            <TransactionTable
+              transactions={transactions}
+              confirmDeleteTransaction={confirmDeleteTransaction}
+              setIsOpen={setIsOpen}
+              setTnxId={setTnxId}
+            />
           </TabsContent>
           <TabsContent value="Expense">
-            <TransactionTable transactions={transactions} confirmDeleteTransaction={confirmDeleteTransaction} setIsOpen={setIsOpen} setTnxId={setTnxId} />
+            <TransactionTable
+              transactions={transactions}
+              confirmDeleteTransaction={confirmDeleteTransaction}
+              setIsOpen={setIsOpen}
+              setTnxId={setTnxId}
+            />
           </TabsContent>
         </div>
         <MyModal isOpen={isOpen} setIsOpen={setIsOpen} txn={tnxId} />
@@ -103,16 +164,23 @@ export default function TransactionsPage() {
   );
 }
 
-function TransactionTable({ transactions, confirmDeleteTransaction, setIsOpen, setTnxId }) {
+function TransactionTable({
+  transactions,
+  confirmDeleteTransaction,
+  setIsOpen,
+  setTnxId,
+}) {
   if (!transactions || transactions.length === 0) {
     return (
       <div className="text-center p-4">
-        <p className="text-gray-500">No transactions have been recorded in the system</p>
+        <p className="text-gray-500">
+          No transactions have been recorded in the system
+        </p>
       </div>
     );
   }
   return (
-    <div className="overflow-x-auto">
+    <div id="transactions-table" className="overflow-x-auto">
       <table className="w-full border-collapse border">
         <thead>
           <tr className="text-left">
@@ -134,9 +202,13 @@ function TransactionTable({ transactions, confirmDeleteTransaction, setIsOpen, s
               <td className="border px-4 py-2">${txn.amount.toFixed(2)}</td>
               <td className="border px-4 py-2">{txn.category}</td>
               <td className="border px-4 py-2">{txn.note}</td>
-              <td className="border px-4 py-2">{new Date(txn.date).toLocaleDateString()}</td>
-              <td className="border px-4 py-2">{new Date(txn.createdAt).toLocaleString()}</td>
-              <td className="border px-4 py-2 ">
+              <td className="border px-4 py-2">
+                {new Date(txn.date).toLocaleDateString()}
+              </td>
+              <td className="border px-4 py-2">
+                {new Date(txn.createdAt).toLocaleString()}
+              </td>
+              <td className="border px-4 py-2">
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
                     <MoreVertical size={18} />
@@ -144,10 +216,19 @@ function TransactionTable({ transactions, confirmDeleteTransaction, setIsOpen, s
                   <DropdownMenuContent>
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={() => { setIsOpen(true); setTnxId(txn._id); }} className="flex items-center cursor-pointer">
+                    <DropdownMenuItem
+                      onClick={() => {
+                        setIsOpen(true);
+                        setTnxId(txn._id);
+                      }}
+                      className="flex items-center cursor-pointer"
+                    >
                       <Edit className="mr-2 h-4 w-4" /> Edit
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => confirmDeleteTransaction(txn._id)} className="flex items-center cursor-pointer">
+                    <DropdownMenuItem
+                      onClick={() => confirmDeleteTransaction(txn._id)}
+                      className="flex items-center cursor-pointer"
+                    >
                       <Delete className="mr-2 h-4 w-4" /> Delete
                     </DropdownMenuItem>
                     <DropdownMenuItem className="flex items-center cursor-pointer">
