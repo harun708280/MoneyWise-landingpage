363051cf277a48ec9d7f2e0fa4d0a0fa9deb5624
harun708280 <harun708280@gmail.com>
2025-05-17 22:19:33 +0600

    update

--- src/redux/Api/baseApi.js
+++ src/redux/Api/baseApi.js
@@ -2,10 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl:'https://moneywise-server.up.railway.app/api'}), 
+  baseQuery: fetchBaseQuery({ baseUrl:'https://moneywise-server-production.up.railway.app/api'}), 
   tagTypes: ["userTransactions","Saving"],  
   endpoints: () => ({}),
 });
+
 // import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // // export const baseApi = createApi({
 //   reducerPath: "baseApi",
